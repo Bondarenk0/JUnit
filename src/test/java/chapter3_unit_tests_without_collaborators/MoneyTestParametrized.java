3e9a050f3e36eef60ae070835a820fa2b4084500
Maksim Bandarenka <mbandarenka@artezio.com>
2019-01-04 11:18:29 +0300

    added parametrized example in chapter 3

---
+++ src/test/java/chapter3_unit_tests_without_collaborators/MoneyTestParametrized.java
@@ -0,0 +1,30 @@
+package chapter3_unit_tests_without_collaborators;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+public class MoneyTestParametrized {
+	
+	private static final Object[] getMoney() {
+		return new Object[] {
+				new Object[] {10, "USD"},
+				new Object[] {20, "EUR"}
+		};
+	}
+
+	@Test
+	@Parameters(method = "getMoney")
+	public void test(int amount, String currency) {
+		Money money = new Money(amount, currency);
+		assertEquals(amount, money.getAmount());
+		assertEquals(currency, money.getCurrency());
+	}
+
+}
