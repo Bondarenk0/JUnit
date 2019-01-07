efacf47e1829ebad75004ad46c84ce293c5cdbe7
Maksim Bandarenka <verian@tut.by>
2019-01-07 19:41:07 +0300

    chapter5 initialized

---
+++ src/main/java/chapter5_FakesObjects/RaceResultsService.java
@@ -0,0 +1,5 @@
+package chapter5_FakesObjects;
+
+public class RaceResultsService {
+
+}
---
+++ src/test/java/chapter5_FakesObjects/RaceResultServiceTest.java
@@ -0,0 +1,15 @@
+package chapter5_FakesObjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RaceResultServiceTest {
+
+	@Test
+	public void subscribedClientShouldReceiveMessage() {
+		RaceResultsService raceResults = new RaceResultsService();
+		//mock
+	}
+
+}
